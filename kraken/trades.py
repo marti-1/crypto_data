e04dcc19ba96da2fd83dca9d0d135fcdb85ce515
marti-1 <martynas.miliauskas@protonmail.com>
2021-06-08 22:16:41 +0300

    store trades in db

--- kraken/trades.py
+++ kraken/trades.py
@@ -2,56 +2,78 @@ import argparse
 import config as cfg
 import requests
 import arrow
-import time
 import json
 from pathlib import Path
 import sys
+import db
+from psycopg2.extras import DictCursor
 
 config = cfg.load()
+
+
+def get_since_param(pair, use_db=False, since=None):
+    if use_db:
+        with db.get_connection() as conn:
+            row = db.one(conn, f'select time from kraken_{pair}_trades order by time desc limit 1')
+            if row is not None:
+                return int(row[0])
+    else:
+        output_filename = get_output_filename(pair)
+        if Path(output_filename).is_file():
+            with open(output_filename, 'r', encoding='utf-8') as f:
+                for line in f:
+                    pass
+            return json.loads(line)['last'] // 1e9
+    return since
+
+
+def get_output_filename(pair):
+    return f"output/{pair}_trades.json"
+
+
+def get_trades(pair, since):
+    acc = []
+    r = requests.post(f"https://api.kraken.com/0/public/Trades?pair={pair}&since={since}")
+    if r.status_code == 200:
+        trades = r.json()['result'][args.pair]
+        if len(trades) == 0:
+            return None
+        for t in trades:
+            acc.append(t)
+    return acc
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('pair', type=str)
+    parser.add_argument('--db', dest='db', action='store_true')
     parser.add_argument('--since', type=int, default=arrow.get('2015-01-01').timestamp)
     parser.add_argument('--to', type=int, default=arrow.get().timestamp)
 
-    args = parser.parse_args()
+    args = parser.parse_args(['XETHZUSD', '--db'])
 
-    since = args.since
     to = args.to
+    since = get_since_param(args.pair, args.db, args.since)
 
-    all_trades = []
-    output_filename = f"output/{args.pair}_trades.json"
-
-    if Path(output_filename).is_file():
-        with open(output_filename, 'r', encoding='utf-8') as f:
-            for line in f:
-                pass
-            last = line
-        since = json.loads(line)['last']
-
-    if args.to and int(since) // 1e9 >= to:
+    if since >= to:
         sys.exit()
 
     while True:
         print(since)
-        r = requests.post(f"https://api.kraken.com/0/public/Trades?pair={args.pair}&since={since}")
-        if r.status_code == 200:
-            trades = r.json()['result'][args.pair]
-            if len(trades) == 0:
-                break
-            for t in trades:
-                all_trades.append(t)
+        trades = get_trades(args.pair, since)
+        if trades is None:
+            break
+        if args.db:
+            with db.get_connection() as conn:
+                cur = conn.cursor(cursor_factory=DictCursor)
+                args_str = b','.join(cur.mogrify("(%s,%s,%s,%s,%s,%s)", x) for x in trades)
+                cur.execute(
+                    f"INSERT INTO kraken_{args.pair}_trades (price,volume,time,buy_sell,market_limit,misc) VALUES " + args_str.decode(
+                        'utf-8'))
+                conn.commit()
+                cur.close()
         else:
+            pass
+        since = int(trades[-1][2])
+        if since >= to:
             break
-        since = int(r.json()['result']['last'])
-
-        with open(output_filename, 'a', encoding='utf-8') as f:
-            json.dump(r.json()['result'], f, ensure_ascii=False)
-            f.write("\n")
-
-        if args.to and since // 1e9 >= to:
-            break
-
-        time.sleep(2)
